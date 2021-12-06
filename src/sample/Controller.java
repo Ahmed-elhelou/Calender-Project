ab87d32f5537f615a463505be9d71f2c08dfcdb6
ahmed-elhelou <ahmedmazenhelou2001@gmail.com>
2021-12-06 19:27:38 +0200

    first commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextField monthField = new TextField();
+    @FXML
+    TextField yearField = new TextField();
+    @FXML
+    Label result = new Label();
+    
+}
